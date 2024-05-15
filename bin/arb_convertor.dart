6726888e987335e8fcd4e4eab0f3202d4014f4c9
ATekimbaev <tekimbaev@gmail.com>
2024-05-15 16:22:22 +0600

    first commit

---
+++ bin/arb_convertor.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'apps_data.dart';
+import 'marketing_data.dart';
+
+void saveToArb(Map<String, dynamic> difference, String filePath) {
+  var arbContent = '{\n  "@@locale": "en",\n';
+  difference.forEach((key, value) {
+    arbContent += '  "$key": "${value.toString()}",\n';
+  });
+  arbContent += '}\n';
+
+  File(filePath).writeAsStringSync(arbContent);
+}
+
+Map<String, dynamic> compareMaps(
+    Map<String, dynamic> map1, Map<String, dynamic> map2) {
+  Map<String, dynamic> difference = {};
+
+  map1.forEach((key, value) {
+    if (!map2.containsKey(key)) {
+      difference[key] = map1[key];
+    }
+  });
+
+  map2.forEach((key, value) {
+    if (!map1.containsKey(key)) {
+      difference[key] = map2[key];
+    }
+  });
+
+  return difference;
+}
+
+void main() {
+  // Сравниваем их и сохраняем различия в файл
+  Map<String, dynamic> difference =
+      compareMaps(MarketingData.marketingData, AppsData.appsData);
+  saveToArb(difference, "difference.arb");
+}
---
+++ test/arb_convertor_test.dart
@@ -0,0 +1,8 @@
+import 'package:arb_convertor/arb_convertor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
